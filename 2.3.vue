eb2b652683850060b0d9d35bcdc5f595e5b2db7d
Prajjwal-creator <139520522+Prajjwal-creator@users.noreply.github.com>
2023-09-28 09:45:29 +0530

    Add files via upload

---
+++ 2.3.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <!-- Place your template syntax code here -->
+        <h1>{{ message }}</h1>
+        <div v-html="htmlContent"></div>
+        <div :id="dynamicId"></div>
+        <p>{{ computedValue }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Text Interpolation",
+            htmlContent: "<strong>Raw HTML</strong>",
+            dynamicId: "myDynamicId",
+        };
+    },
+    computed: {
+        computedValue() {
+            return "Computed Value";
+        },
+    },
+};
+</script>
+
+<style scoped>
+/* Add scoped CSS here or import an external CSS file */
+</style>
